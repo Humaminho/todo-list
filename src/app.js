c7f8e5e1884cefbaafc12f394e861c4f61fda3e2
Humam Kharbouch <hu666mam@gmail.com>
2023-03-01 22:18:45 +0100

    creating projects

--- src/app.js
+++ src/app.js
@@ -1,21 +1,53 @@
 import './styles.css'
-const projects = [];
-class Section {
+const projectsArray = []; // PROJECTS ARRAY
+const allTasks = []; // TASKS
+const log = console.log;
+const projectsEl = document.getElementById('projects'); // PROJECTS DOM
+class Project {
   constructor(name) {
     this.name = name;
     this.tasks = [];
-    this.isProject = false;
   } 
 }
 
-class Project extends Section {
-  constructor(name) {
-    super(name);
-    this.isProject = true;
-  }
+function createProject(name) { // CREATE PROJECT
+  const project = new Project(name);
+  projectsArray.push(project);
 }
 
-function createProject(name) {
-  const project = new Project(name);
-  projects.push(project);
+function renderProjects(projects) { // RENDER PROJECTS
+  projectsEl.innerHTML = '';
+  for ( const project of projects ) {
+    const projectIndex = projectsArray.indexOf(project);
+    const projectEl = document.createElement('div');
+    projectEl.classList.add('project');
+    projectEl.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-list-task" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z"/>
+      <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z"/>
+      <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z"/>
+    </svg>
+    <p>${project.name}</p>
+    <div id="delete-project" data-name="${projectIndex}">
+      <svg data-name="${projectIndex}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+        <path data-name="${projectIndex}" d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+      </svg>
+    </div>`;
+    projectsEl.appendChild(projectEl);
+    const deleteProjectBtn = document.getElementById('delete-project');
+    console.log(deleteProjectBtn);
+    deleteProjectBtn.addEventListener('click', (e) => {
+      log(e.target.dataset.name);
+      // const index = projectsArray.indexOf(project);
+      // projectsArray.splice(index, 1);
+      // console.log(projectsArray);
+      // renderProjects(projectsArray);
+    }, { capture: true });
+  }
 }
+const addProjectBtn = document.getElementById('add-project');
+addProjectBtn.addEventListener('click', () => {
+  const projectName = prompt('Enter project name');
+  createProject(projectName);
+  renderProjects(projectsArray);
+});

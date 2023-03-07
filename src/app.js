53874f31b57a2acf68aafc7a6a601f301998cd4c
Humam Kharbouch <hu666mam@gmail.com>
2023-03-07 22:21:38 +0100

    checkpointo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const allTasks = []; // TASKS
 let currentProject = 0; // CURRENT PROJECT
 const log = console.log;
 const projectsEl = document.getElementById('projects'); // PROJECTS DOM
+const tasksEl = document.getElementById('tasks'); // TASKS DOM
 class Project {
   constructor(name) {
     this.name = name;
@@ -100,9 +101,9 @@ const addTaskBtn = document.getElementById('add-task');
 addTaskBtn.addEventListener('click', swapSections);
 
 function submitTask() {
-  const taskTitle = document.getElementById('task-title').value;
-  const taskDescription = document.getElementById('task-description').value;
-  const taskDueDate = document.getElementById('task-due-date').value;
+  const taskTitle = document.getElementById('task-title');
+  const taskDescription = document.getElementById('task-description');
+  const taskDueDate = document.getElementById('task-due-date');
   const radioBtns = document.querySelectorAll('input[type="radio"]');
   let taskPriority = '';
   for (let i = 0; i < radioBtns.length; i++) {
@@ -110,17 +111,42 @@ function submitTask() {
       taskPriority = radioBtns[i].value;
     }
   }
-  if (taskTitle === '' || taskDescription === '' || taskDueDate === '' || taskPriority === '') {
+  if (taskTitle.value === '' || taskDescription.value === '' || taskDueDate.value === '' || taskPriority === '') {
     alert('Please fill all fields');
     return;
   }
-  const task = new Task(taskTitle, taskDescription, taskDueDate, taskPriority, currentProject);
+  const task = new Task(taskTitle.value, taskDescription.value, taskDueDate.value, taskPriority, currentProject);
   allTasks.push(task);
+  console.log(task)
   //currentProject.tasks.push(task);
   swapSections();
+  renderTasks(allTasks);
+  taskTitle.value = '';
+  taskDescription.value = '';
+  taskDueDate.value = '';
+  taskPriority = '';
 }
 
 const submitTaskBtn = document.getElementById('submit-task');
 submitTaskBtn.addEventListener('click', submitTask);
 
 addProjectSelectionEvent();
+
+function renderTasks(tasks) {
+  const taskEl = document.createElement('div');
+  taskEl.classList.add('task');
+  for (let i = 0; i < tasks.length; i++) {
+    taskEl.innerHTML = `
+      <div class="check ${tasks[i].priority}"></div>
+      <div class="task-title">${tasks[i].name}</div>
+      <div class="time">${tasks[i].dueDate}</div>
+      <svg class="task-info" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+      </svg>
+      <svg class="remove-task" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+      </svg>`;
+  }
+  tasksEl.appendChild(taskEl);
+}
